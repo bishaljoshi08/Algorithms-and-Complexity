0fbde206cae96132f0ed94777b43de33692f29d3
Bishal Joshi <bishaljoshi08@gmail.com>
2022-03-02 21:42:59 +0545

    complete lab 1

--- lab1/time.py
+++ lab1/time.py
@@ -1,70 +1,51 @@
+from cProfile import label
 from binary import binary_search
 from linear import linear_search
 import time
 import matplotlib.pyplot as plt
-import numpy
-import math
 
+_, (plt1, plt2) = plt.subplots(nrows=1, ncols=2)
 
-t=[]
-y = []
-a=list(range(1000,1000000,10000))
+wt = []
+a = range(100, 100000, 100)
 for i in a:
     start = time.time()
-    binary_search(list(range(i)), 0, i-1, -1) #worstcase of binary_search- either not in array or extreme points
+    binary_search(range(i), 0, i-1, i)
     end = time.time()
-    t.append([end-start])
-    y.append(math.log(i,2))
-print(t)
-plt.plot(a, t,'.')
-m,b = numpy.polyfit(a, t,1)
-plt.plot(a,m*a+b,label='Worst case Binary Search')
-plt.plot(a,y, label='O(logn)')
-t.clear()
-y.clear()
+    wt.append((end-start))
+plt1.plot(a, wt,".", label="Worst Case")
+
+bt = []
 for i in a:
     start = time.time()
-    binary_search(list(range(i)),0, i-1, i//2)
+    binary_search(range(i), 0, i-1, (i-1)//2)
     end = time.time()
-    t.append([end-start])
-    y.append(1)
-plt.plot(a, t,'.')
-m,b = numpy.polyfit(a, t,1)
-plt.plot(a,m*a+b,label='Best case Binary Search') #bestcase of binary_search- mid element
-plt.plot(a,y, label='O(1)')
-t.clear()
-y.clear()
+    bt.append((end-start))
+plt1.plot(a, bt, "+", label="Best Case")
+
+plt1.set_title("Binary Search")
+plt1.set_xlabel("Length of Array")
+plt1.set_ylabel("Time")
+
+plt1.legend()
+
+bt= []
 for i in a:
     start = time.time()
-    linear_search(list(range(i)),0)
+    linear_search(range(i),0)
     end = time.time()
-    t.append([end-start])
-plt.plot(a, t,'.')
-m,b = numpy.polyfit(a, t,1)
-plt.plot(a,m*a+b,label='Best Case Linear Search') #best case of linear search - first element
+    bt.append(end-start)
+plt2.plot(a, bt,'.',label= 'Best Case')
 
-t.clear()
-y.clear()
+wt=[]
 for i in a:
     start = time.time()
-    linear_search(list(range(i)),i+1)
+    linear_search(range(i),i)
     end = time.time()
-    t.append([end-start])
-    y.append(i)
-plt.plot(a, t,'.')
-m,b = numpy.polyfit(a, t,1)
-plt.plot(a,m*a+b,label='Worst case Linear Search') #worst case of linear search - last element or greater
-plt.plot(a,y, label='O(n)')
+    wt.append(end-start)
+plt2.plot(a, wt,'+', label='Worst Case')
+plt2.set_title("Linear Search")
+plt2.set_xlabel("Length of Array")
+plt2.set_ylabel("Time")
 
-plt.xlabel('length of array')
-
-plt.ylabel('time')
-
-
-plt.legend()
 plt.show()
-
-
- 
-# t.clear()
-# print(t)
