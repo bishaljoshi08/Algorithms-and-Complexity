7b8be8d3c87aac9726f4efbf12313baef7e4dbb1
Shreyam Pokharel <pshreyam@gmail.com>
2022-03-06 17:59:01 +0545

    Fix plot legend and generate images

--- lab1/time.py
+++ lab1/time.py
@@ -1,51 +1,59 @@
-from cProfile import label
+import time
+
+import matplotlib.pyplot as plt
+
 from binary import binary_search
 from linear import linear_search
-import time
-import matplotlib.pyplot as plt
+
 
 _, (plt1, plt2) = plt.subplots(nrows=1, ncols=2)
 
-wt = []
 a = range(100, 100000, 100)
-for i in a:
-    start = time.time()
-    binary_search(range(i), 0, i-1, i)
-    end = time.time()
-    wt.append((end-start))
-plt1.plot(a, wt,".", label="Worst Case")
+
+# Time Complexity for Binary Search
 
 bt = []
 for i in a:
     start = time.time()
     binary_search(range(i), 0, i-1, (i-1)//2)
     end = time.time()
-    bt.append((end-start))
+    bt.append(end-start)
 plt1.plot(a, bt, "+", label="Best Case")
+
+wt = []
+for i in a:
+    start = time.time()
+    binary_search(range(i), 0, i-1, i)
+    end = time.time()
+    wt.append(end-start)
+plt1.plot(a, wt, ".", label="Worst Case")
 
 plt1.set_title("Binary Search")
 plt1.set_xlabel("Length of Array")
-plt1.set_ylabel("Time")
-
+plt1.set_ylabel("Time (in secs)")
 plt1.legend()
 
-bt= []
+# Time Complexity for Linear Search
+
+bt = []
 for i in a:
     start = time.time()
-    linear_search(range(i),0)
+    linear_search(range(i), 0)
     end = time.time()
     bt.append(end-start)
-plt2.plot(a, bt,'.',label= 'Best Case')
+plt2.plot(a, bt, "+", label="Best Case")
 
-wt=[]
+wt = []
 for i in a:
     start = time.time()
-    linear_search(range(i),i)
+    linear_search(range(i), i)
     end = time.time()
     wt.append(end-start)
-plt2.plot(a, wt,'+', label='Worst Case')
+plt2.plot(a, wt, ".", label="Worst Case")
+
 plt2.set_title("Linear Search")
 plt2.set_xlabel("Length of Array")
-plt2.set_ylabel("Time")
+plt2.set_ylabel("Time (in secs)")
+plt2.legend()
 
 plt.show()
